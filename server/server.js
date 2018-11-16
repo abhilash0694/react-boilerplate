e9ac2e69bff6d5c239703367f82143012174bf24
Abhilash Pandey <abhilash.pandey@reverieinc.com>
2018-11-16 11:30:38 +0530

    Revert "Initial Application Commit for Dev Env Setup"
    This reverts commit 866745f4f9275e9e4d9e408c1dbf5963faa3f300.

--- server/server.js
+++ server/server.js
@@ -1,37 +1,34 @@
 
-const express = require('express');
-const webpack = require('webpack');
-const path = require('path');
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
 //pulling in dependencies that we will need.
-const config = require('../webpack.config.dev');
-const open = require('open');
+import config from '../webpack.config.dev';
+import open from 'open';
 /* eslint-disable no-console */
 //specify  the server port
 const port = 3007;
 //creating a instance of express
+const app = express();
+//calls webpack with the dev configuartion that we created
+const compiler = webpack(config);
+// tell express to use webpack-dev-middleware, and we pass it our compiled webpack configuration
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
-  const app = express();
-  //calls webpack with the dev configuartion that we created
-  const compiler = webpack(config);
-  module.exports = () => {
-  // tell express to use webpack-dev-middleware, and we pass it our compiled webpack configuration
-  app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  }));
-
-  //use webpack-hot-middleware and pass it the compiled webpack cinfigurations.
-  app.use(require('webpack-hot-middleware')(compiler));
-  // /express to serve up our index.html for any request on the server.
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../src/index.html'));
-  });
-  //starts up express listening on the port configured above. 
-  app.listen(port, function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      open(`http://localhost:${port}`);
-    }
-  });
-}
+//use webpack-hot-middleware and pass it the compiled webpack cinfigurations.
+app.use(require('webpack-hot-middleware')(compiler));
+// /express to serve up our index.html for any request on the server.
+app.get('*', function(req, res) {
+  res.sendFile(path.join( __dirname, '../src/index.html'));
+});
+//starts up express listening on the port configured above. 
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,41 +1,39 @@
-const webpack = require('webpack');
-const path = require('path');
+import webpack from 'webpack';
+import path from 'path';
 
 
 
-module.exports = {
-  debug: true,//sets debug to true, this will enable the display of debug information.
-  devtool: 'inline-source-map',//this set the devtool to inline-source-map
-  noInfo: false,//this sets noInfo to false and webpack will display a list of all the files that it’s bundling.
-  entry: [// defining our applications entry point.
-    'eventsource-polyfill',
-    'webpack-hot-middleware/client?reload=true',
-    path.resolve(__dirname, 'src/index')//actual entry point.
-  ],
-  target: 'web',
-  output: {//specifies where webpack will create our dev bundle,
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {//webpack’s devServer where our source code lives
-    contentBase: path.resolve(__dirname, 'src')
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  module: {
-    loaders: [//all file application handles by webpack
-      { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'] },
-      { test: /(\.css)$/, loaders: ['style', 'css'] },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
+export default{
+    debug:true,//sets debug to true, this will enable the display of debug information.
+    devtool:'inline-source-map',//this set the devtool to inline-source-map
+    noInfo:false,//this sets noInfo to false and webpack will display a list of all the files that it’s bundling.
+    entry: [// defining our applications entry point.
+        'eventsource-polyfill',
+        'webpack-hot-middleware/client?reload=true',
+        path.resolve(__dirname, 'src/index')//actual entry point.
+      ],
+      target: 'web',
+      output: {//specifies where webpack will create our dev bundle,
+        path: __dirname + '/dist', 
+        publicPath: '/',
+        filename: 'bundle.js'
       },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
-    ]
-  }
+      devServer: {//webpack’s devServer where our source code lives
+        contentBase: path.resolve(__dirname, 'src')
+      },
+      plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+      ],
+      module: {
+        loaders: [//all file application handles by webpack
+          {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+          {test: /(\.css)$/, loaders: ['style', 'css']},
+          {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+          {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+          {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+           loader: 'url?limit=10000&mimetype=application/octet-stream'},
+          {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+        ]
+      }
 }
